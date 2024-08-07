654e7debe5f5e907ccc3e8fc472b65f01eb6e7fc
MahabirGupta <mahabirg81@gmail.com>
2024-08-07 16:50:11 +0800

    first commit

---
+++ src/main/java/com/example/SpringJDBCProj/SpringJdbcProjApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringJDBCProj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJdbcProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJdbcProjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringJDBCProj/SpringJdbcProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringJDBCProj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJdbcProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
